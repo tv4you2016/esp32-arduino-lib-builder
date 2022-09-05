1a4e83d5232643ffbe996774f395258b23f4dda1
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-05 16:44:29 +0200

    no duplicate clone of esptool v4.x

--- tools/copy-bootloader.sh
+++ tools/copy-bootloader.sh
@@ -14,7 +14,10 @@ mkdir -p "$AR_SDK/bin"
 # Workaround for getting the bootloaders to be flashable with esptool v4.x
 # It might still be needed for IDF5, but using the included esptool instead
 #cp "build/bootloader/bootloader.bin" "$AR_SDK/bin/bootloader_$BOOTCONF.bin"
-if [ ! -e "tools/esptool" ]; then
-	git clone https://github.com/espressif/esptool tools/esptool
-fi
-./tools/esptool/esptool.py --chip "$IDF_TARGET" elf2image --dont-append-digest "build/bootloader/bootloader.elf" -o "$AR_SDK/bin/bootloader_$BOOTCONF.bin"
+
+# We use esptool v.4.21 in Tasmota IDF fork so no need to clone here
+
+#if [ ! -e "tools/esptool" ]; then
+#	git clone https://github.com/espressif/esptool tools/esptool
+#fi
+./esp-idf/components/esptool_py/esptool/esptool.py --chip "$IDF_TARGET" elf2image --dont-append-digest "build/bootloader/bootloader.elf" -o "$AR_SDK/bin/bootloader_$BOOTCONF.bin"
