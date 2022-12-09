fd8c2a3b46f4b55c87646626bbe469256955678a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-12-09 22:06:06 +0100

    esp32-arduino-lib-builder-INFO.txt

--- tools/config.sh
+++ tools/config.sh
@@ -36,7 +36,7 @@ AR_REPO="$AR_USER/arduino-esp32"
 # Arduino branch to use
 AR_BRANCH="master"
 
-echo "Framework built from IDF branch $IDF_BRANCH and Arduino branch $AR_BRANCH" >> INFO.txt
+echo "Framework built from IDF branch $IDF_BRANCH and Arduino branch $AR_BRANCH" >> esp32-arduino-lib-builder-INFO.txt
 
 AR_REPO_URL="https://github.com/$AR_REPO.git"
 if [ -n $GITHUB_TOKEN ]; then
