1e9f62721cbcc6a8e166a53cac9e36563a22f113
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-12-10 18:59:59 +0100

    temp fix for changed bootloader handling

--- tools/copy-bootloader.sh
+++ tools/copy-bootloader.sh
@@ -8,7 +8,9 @@ BOOTCONF=$FLASH_MODE"_$FLASH_FREQ"
 source ./tools/config.sh
 
 echo "Copying bootloader: $AR_SDK/bin/bootloader_$BOOTCONF.elf"
+echo "Copying bootloader: $AR_SDK/bin/bootloader_$BOOTCONF.bin"
 
 mkdir -p "$AR_SDK/bin"
 
 cp "build/bootloader/bootloader.elf" "$AR_SDK/bin/bootloader_$BOOTCONF.elf"
+./esp-idf/components/esptool_py/esptool/esptool.py --chip "$IDF_TARGET" elf2image --dont-append-digest "build/bootloader/bootloader.elf" -o "$AR_SDK/bin/bootloader_$BOOTCONF.bin"
