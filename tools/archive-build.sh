a237c35422f569d4380ccec0610dec08d8074150
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-12-06 22:05:34 +0100

    V5.1 git clone optimize

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -23,6 +23,7 @@ cp -f ../components/arduino/CMa* arduino-esp32
 cp -f ../components/arduino/idf* arduino-esp32
 cp -f ../components/arduino/Kco* arduino-esp32
 cp -f ../components/arduino/pac* arduino-esp32
+rm -rf arduino-esp32/idf_component_examples
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/tests
 rm -rf arduino-esp32/idf_component_examples
