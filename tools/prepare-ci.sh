703fe38d6caa1cd794be633302f865426118e7a8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-01-31 22:06:35 +0100

    Use MacOS ARM

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install -y gperf cmake ninja-build
-pip3 install wheel future pyelftools
+# Ubuntu setup
+#sudo apt-get install -y gperf cmake ninja-build
+#pip3 install wheel future pyelftools
+
+# MacOS (ARM) setup
+brew install gsed
+brew install gawk
+brew install gperf
+brew install ninja
+brew install ccache
+python -m pip install --upgrade pip
+pip install wheel future pyelftools 
