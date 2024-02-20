242560618ee31d284d1274b8f77fa472e8c0df6c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-20 18:42:04 +0100

    Update archive-build.sh

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD || echo "")
-IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || git -C "$IDF_PATH" tag --points-at HEAD || echo "")
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 
 archive_path="dist/arduino-esp32-libs-ITEAD-$idf_version_string.tar.gz"
