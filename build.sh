24cbf60a6b8a506f0c37140a86a37a61e7a2ae1f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-21 17:30:14 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -252,8 +252,9 @@ fi
 
 # Generate PlatformIO framework manifest file
 rm -rf "$AR_ROOT/package.json"
+AR_VERSION=$(jq -c '.version' "$AR_COMPS/arduino/package.json" | tr -d '"')
 if [ "$BUILD_TYPE" = "all" ]; then
-    python3 ./tools/gen_pio_frmwk_manifest.py -o "$AR_ROOT/" -s "$IDF_BRANCH" -c "$IDF_COMMIT"
+    python3 ./tools/gen_pio_frmwk_manifest.py -o "$AR_ROOT/" -s "v$AR_VERSION" -c "$IDF_COMMIT"
     if [ $? -ne 0 ]; then exit 1; fi
 fi
 
