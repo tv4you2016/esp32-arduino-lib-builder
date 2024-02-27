00acd83b84760dff5d56fd7728bd29856a45c213
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-27 21:20:51 +0100

    Update prepare-ci.sh

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -2,7 +2,7 @@
 
 # Ubuntu setup
 # Change in archive-build.sh gawk to awk
-#sudo apt-get install -y gperf cmake ninja-build
+#sudo apt update && sudo apt install -y gperf cmake ninja-build
 #pip3 install wheel future pyelftools
 
 # MacOS (ARM) setup
