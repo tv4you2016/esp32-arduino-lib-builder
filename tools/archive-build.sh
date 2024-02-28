de3cda4338ae70f928df8c2e3f35c7a68ed563b4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-02-28 15:42:58 +0100

    Update archive-build.sh

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD || echo "")
+IDF_Commit_short=${IDF_Commit::7}
 IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD || git -C "$IDF_PATH" tag --points-at HEAD || echo "")
 
-idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
+idf_version_string=${IDF_BRANCH//\//_}"-$IDF_Commit_short"
 archive_path="dist/arduino-esp32-libs-$idf_version_string.tar.gz"
 build_archive_path="dist/arduino-esp32-build-$idf_version_string.tar.gz"
 pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string.tar.gz"
