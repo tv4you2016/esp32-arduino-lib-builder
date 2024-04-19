ec7130fabcbaa90489e09c123842f98c73829fe3
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-19 17:26:45 +0200

    remove TinyUSB

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -3,8 +3,6 @@
 source ./tools/config.sh
 
 CAMERA_REPO_URL="https://github.com/espressif/esp32-camera.git"
-TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
-TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 
 #
 # CLONE/UPDATE ESP32-CAMERA
@@ -26,17 +24,3 @@ if [ $? -ne 0 ]; then exit 1; fi
 # Arduino needs cam_hal.h from esp32-camera in include folder
 #
 cp "$AR_COMPS/esp32-camera/driver/private_include/cam_hal.h" "$AR_COMPS/esp32-camera/driver/include/"
-
-#
-# CLONE/UPDATE TINYUSB
-#
-echo "Updating TinyUSB..."
-if [ ! -d "$TINYUSB_REPO_DIR" ]; then
-       git clone -b master --depth 1 "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
-else
-       cd $TINYUSB_REPO_DIR
-       git pull
-       # -ff is for cleaning untracked files as well as submodules
-       git clean -ffdx
-fi
-if [ $? -ne 0 ]; then exit 1; fi
