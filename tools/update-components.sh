05431c63b9d4aeb2d403ec0dc236592cdc0e983d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-10 23:17:22 +0200

    Webcam component from registry

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -2,30 +2,8 @@
 
 source ./tools/config.sh
 
-CAMERA_REPO_URL="https://github.com/espressif/esp32-camera.git"
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
-
-#
-# CLONE/UPDATE ESP32-CAMERA
-#
-echo "Updating ESP32 Camera..."
-if [ ! -d "$AR_COMPS/esp32-camera" ]; then
-       git clone -b master --recursive --depth 1 --shallow-submodule $CAMERA_REPO_URL "$AR_COMPS/esp32-camera"
-else
-       cd "$AR_COMPS/esp32-camera"
-       git pull
-       git submodule update --depth 1
-       # -ff is for cleaning untracked files as well as submodules
-       git clean -ffdx
-       cd -
-fi
-if [ $? -ne 0 ]; then exit 1; fi
-
-#
-# Arduino needs cam_hal.h from esp32-camera in include folder
-#
-cp "$AR_COMPS/esp32-camera/driver/private_include/cam_hal.h" "$AR_COMPS/esp32-camera/driver/include/"
 
 #
 # CLONE/UPDATE TINYUSB
