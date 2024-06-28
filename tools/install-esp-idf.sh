c6714dd5ba536e6855e2d2285b030bc7f669b8f4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-06-28 11:50:02 +0200

    Add temporary fix for mmu map and late init of psram

--- tools/install-esp-idf.sh
+++ tools/install-esp-idf.sh
@@ -64,9 +64,11 @@ if [ ! -x $idf_was_installed ] || [ ! -x $commit_predefined ]; then
         git submodule update --recursive
 	$IDF_PATH/install.sh
 
-	# Temporarily patch the ESP32-S2 I2C LL driver to keep the clock source
+	# 1) Temporarily patch the ESP32-S2 I2C LL driver to keep the clock source
+        # 2) Temporarily fix for mmu map and late init of psram https://github.com/espressif/arduino-esp32/issues/9936
 	cd $IDF_PATH
 	patch -p1 -N -i ../patches/esp32s2_i2c_ll_master_init.diff
+        patch -p1 -N -i ../patches/mmu_map.diff
 	cd -
 
         # Get the exact IDF version from file "version.txt"
