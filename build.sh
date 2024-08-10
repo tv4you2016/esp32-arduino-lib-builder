e1959df8a12b8b7d3f55f8f503eaaae7e2240c7c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-10 19:18:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -197,6 +197,9 @@ for target_json in `jq -c '.targets[]' configs/builds.json`; do
 
     echo "* Build IDF-Libs: $idf_libs_configs"
     rm -rf build sdkconfig
+    pushd $AR_MANAGED_COMPS
+    rm -- **/.component_hash
+    popd
     idf.py -DIDF_TARGET="$target" -DSDKCONFIG_DEFAULTS="$idf_libs_configs" idf-libs
     if [ $? -ne 0 ]; then exit 1; fi
 
@@ -209,6 +212,9 @@ for target_json in `jq -c '.targets[]' configs/builds.json`; do
 
         echo "* Build BootLoader: $bootloader_configs"
         rm -rf build sdkconfig
+        pushd $AR_MANAGED_COMPS
+        rm -- **/.component_hash
+        popd
         idf.py -DIDF_TARGET="$target" -DSDKCONFIG_DEFAULTS="$bootloader_configs" copy-bootloader
         if [ $? -ne 0 ]; then exit 1; fi
     done
@@ -222,6 +228,9 @@ for target_json in `jq -c '.targets[]' configs/builds.json`; do
 
         echo "* Build Memory Variant: $mem_configs"
         rm -rf build sdkconfig
+        pushd $AR_MANAGED_COMPS
+        rm -- **/.component_hash
+        popd
         idf.py -DIDF_TARGET="$target" -DSDKCONFIG_DEFAULTS="$mem_configs" mem-variant
         if [ $? -ne 0 ]; then exit 1; fi
     done
