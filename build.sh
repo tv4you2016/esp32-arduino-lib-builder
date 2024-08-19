10927da4ce77019c4063c06e0f21e5eeeed17363
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-19 18:38:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ fi
 # Fixes building some components. See https://github.com/espressif/arduino-esp32/issues/10167
 export IDF_COMPONENT_OVERWRITE_MANAGED_COMPONENTS=1
 
-export TARGET="all"
+export TARGET="esp32c3"
 BUILD_TYPE="all"
 SKIP_ENV=0
 COPY_OUT=0
