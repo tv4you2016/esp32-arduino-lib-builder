f7486ab6a580bbdf51aaed03539c0737ddc39ee4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-19 17:23:00 +0200

    Arduinon3.1 based on IDF 5.3

--- tools/config.sh
+++ tools/config.sh
@@ -6,12 +6,12 @@ if [ -z $IDF_PATH ]; then
 fi
 
 if [ -z $IDF_BRANCH ]; then
-    export IDF_BRANCH="release/v5.2"
+    export IDF_BRANCH="release/v5.3"
 fi
 
 # Arduino branch to use
 if [ -z $AR_BRANCH ]; then
-    AR_BRANCH="release/v5.2"
+    AR_BRANCH="release/v3.1.x"
 fi
 
 if [ -z $IDF_TARGET ]; then
