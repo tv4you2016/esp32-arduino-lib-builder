e6d4422049a7f7186977aa4afe1a2b8d7f50e74b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-21 10:59:10 +0200

    Update tusb_config.h

--- components/arduino_tinyusb/include/tusb_config.h
+++ components/arduino_tinyusb/include/tusb_config.h
@@ -75,11 +75,9 @@ extern "C" {
 /*                      */
 /* COMMON CONFIGURATION */
 /*                      */
-
-#ifdef CFG_TUSB_MCU
-#   undef CFG_TUSB_MCU
+#ifndef CFG_TUSB_MCU
+#define CFG_TUSB_MCU				OPT_MCU_ESP32S2
 #endif
-#define CFG_TUSB_MCU				        OPT_MCU_ESP32S2
 #define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
 #define CFG_TUSB_OS                 OPT_OS_FREERTOS
 
