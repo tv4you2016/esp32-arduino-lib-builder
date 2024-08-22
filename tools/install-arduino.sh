c1b4cb00257aaf390338e02febd153cecc8221ce
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-23 00:05:30 +0200

    Update install-arduino.sh

--- tools/install-arduino.sh
+++ tools/install-arduino.sh
@@ -69,28 +69,3 @@ if [ -z $AR_BRANCH ]; then
 fi
 
 if [ $? -ne 0 ]; then exit 1; fi
-
-#
-# remove code and libraries not needed/wanted for Tasmota framework
-#
-rm -rf "$AR_COMPS/arduino/docs"
-rm -rf "$AR_COMPS/arduino/idf_component_examples"
-rm -rf "$AR_COMPS/arduino/package"
-rm -rf "$AR_COMPS/arduino/tests"
-rm -rf "$AR_COMPS/arduino/cores/esp32/chip-debug-report.cpp"
-rm -rf "$AR_COMPS/arduino/cores/esp32/chip-debug-report.h"
-rm -rf "$AR_COMPS/arduino/libraries/RainMaker"
-rm -rf "$AR_COMPS/arduino/libraries/Insights"
-rm -rf "$AR_COMPS/arduino/libraries/ESP_I2S"
-rm -rf "$AR_COMPS/arduino/libraries/SPIFFS"
-rm -rf "$AR_COMPS/arduino/libraries/BLE"
-rm -rf "$AR_COMPS/arduino/libraries/SimpleBLE"
-rm -rf "$AR_COMPS/arduino/libraries/BluetoothSerial"
-rm -rf "$AR_COMPS/arduino/libraries/WiFiProv"
-rm -rf "$AR_COMPS/arduino/libraries/WiFiClientSecure"
-rm -rf "$AR_COMPS/arduino/libraries/NetworkClientSecure"
-rm -rf "$AR_COMPS/arduino/libraries/ESP32"
-rm -rf "$AR_COMPS/arduino/libraries/ESP_SR"
-rm -rf "$AR_COMPS/arduino/libraries/ESP_NOW"
-rm -rf "$AR_COMPS/arduino/libraries/TFLiteMicro"
-rm -rf "$AR_COMPS/arduino/libraries/OpenThread"
