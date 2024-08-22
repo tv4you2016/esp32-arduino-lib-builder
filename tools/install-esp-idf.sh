459fbce816bfe9070531136dd5d19d216f7aac5a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-22 23:53:21 +0200

    Update install-esp-idf.sh

--- tools/install-esp-idf.sh
+++ tools/install-esp-idf.sh
@@ -70,6 +70,7 @@ if [ ! -x $idf_was_installed ] || [ ! -x $commit_predefined ]; then
 	patch -p1 -N -i $AR_PATCHES/esp32s2_i2c_ll_master_init.diff
         patch -p1 -N -i $AR_PATCHES/mmu_map.diff
 	patch -p1 -N -i $AR_PATCHES/lwip_max_tcp_pcb.diff
+        patch -p1 -N -i $AR_PATCHES/platformio-build.diff
 	cd -
 
         # Get the exact IDF version from file "version.txt"
