52b75679940904cb792c3346e61aad4bbd0c86b9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-23 00:44:57 +0200

    Update archive-build.sh

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -24,6 +24,8 @@ cp -f ../components/arduino/pac* arduino-esp32
 rm -rf arduino-esp32/docs
 rm -rf arduino-esp32/tests
 rm -rf arduino-esp32/idf_component_examples
+rm -rf arduino-esp32/libraries/RainMaker
+rm -rf arduino-esp32/libraries/Insights
 rm -rf arduino-esp32/tools/*.exe
 rm -rf arduino-esp32/tools/esptool.py
 rm -rf arduino-esp32/tools/get.py
