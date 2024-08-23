b39d328909d3c491995300eb4973fa5dda05d114
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-23 12:25:36 +0200

    Create get_idf_ver.py

---
+++ tools/get_idf_ver.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+import re
+import sys
+
+def main(ver_file_path):
+    with open(ver_file_path, encoding="utf8") as fp:
+        pattern = r"set\(IDF_VERSION_(MAJOR|MINOR|PATCH) (\d+)\)"
+        matches = re.findall(pattern, fp.read())
+        idf_version = str(".".join([match[1] for match in matches]))
+        os.environ["IDF_VERSION"] = idf_version
+        return idf_version
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        dest="ver_file_path",
+        required=True,
+        help="Full path to the ESP-IDF cmake version file \"version.cmake\"",
+    )
+    args = parser.parse_args()
+
+    sys.exit(main(args.ver_file_path))
